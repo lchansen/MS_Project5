92ac74269c3cd9f39756b948ae8f5ad80f32479b
Lucas Hansen <lchansen97@gmail.com>
2017-10-25 00:46:09 -0500

    Initial Commit

---
+++ BLE Lab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLE Lab
+//
+//  Created by Luke Hansen on 10/25/17.
+//  Copyright © 2017 Luke Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
